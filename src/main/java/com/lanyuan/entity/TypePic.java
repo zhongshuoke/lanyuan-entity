cf87a177ec8a41c018ffaf3b05ed0aa6f8fb3a51
freetiger <freetiger.cn@gmail.com>
2015-03-29 11:22:20 +0800

    栏目图片pojo

--- src/main/java/com/lanyuan/entity/TypePic.java
+++ src/main/java/com/lanyuan/entity/TypePic.java
@@ -1,7 +1,7 @@
 package com.lanyuan.entity;
 
 /**
- * 类别图片实体类
+ * 类别（栏目）图片实体类
  * @author caokun
  *
  */
@@ -9,6 +9,12 @@ public class TypePic {
 
     //类别图片的id
 	private int id;
+	
+	//栏目id
+	private Long wxAccTypeId;
+	
+	//微信文章id
+	private Long wxArticleId;
 	
     //类别图片的服务器地址
 	private String typePicUrl;
@@ -28,4 +34,21 @@ public class TypePic {
 	public void setTypePicUrl(String typePicUrl) {
 		this.typePicUrl = typePicUrl;
 	}
+
+	public Long getWxArticleId() {
+		return wxArticleId;
+	}
+
+	public void setWxArticleId(Long wxArticleId) {
+		this.wxArticleId = wxArticleId;
+	}
+
+	public Long getWxAccTypeId() {
+		return wxAccTypeId;
+	}
+
+	public void setWxAccTypeId(Long wxAccTypeId) {
+		this.wxAccTypeId = wxAccTypeId;
+	}
+	
 }
